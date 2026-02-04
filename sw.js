0a3b3c078e6699c1d7a58792bf7c22269c2fb473
iarun007 <i.arun007@gmail.com>
2026-02-04 15:18:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const cacheName = 'ongc-v3'; // <--- Increment this every time you update code
-const assets = ['./', './index.html', './style.css', './app.js', './data.js', './manifest.json'];
+const cacheName = 'ongc-v10';
+// Only cache essential files to prevent timeout
+const assets = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './data.js',
+    './manifest.json'
+];
 
 self.addEventListener('install', e => {
-    self.skipWaiting(); // Forces the waiting service worker to become active
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('activate', e => {
-    // Clean up old caches
     e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => { if (key !== cacheName) return caches.delete(key); })
-        ))
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(assets);
+        }).catch(err => console.log("Cache error:", err))
     );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
