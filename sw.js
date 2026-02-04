3a72c553b15e692e3ebef4e6e92bd0a0d46b80d6
iarun007 <i.arun007@gmail.com>
2026-02-04 12:48:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const cacheName = 'ongc-v2';
+const cacheName = 'ongc-v2'; // <--- Increment this every time you update code
 const assets = ['./', './index.html', './style.css', './app.js', './data.js', './manifest.json'];
 
 self.addEventListener('install', e => {
+    self.skipWaiting(); // Forces the waiting service worker to become active
     e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+    // Clean up old caches
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => { if (key !== cacheName) return caches.delete(key); })
+        ))
+    );
 });
 
 self.addEventListener('fetch', e => {
